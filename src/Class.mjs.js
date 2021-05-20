c4c7af7f09b2a7fb8b7fe6df6147befeceb2b7d7
wizard04wsu <wizard04@gmail.com>
2021-05-20 13:23:16 -0400

    replaced older src and class tests with rewrites

--- src/Class.mjs.js
+++ src/Class.mjs.js
@@ -1,166 +1,131 @@
 /* eslint-env es6 */
 // https://github.com/wizard04wsu/Class
 
-let _initializing = false;
+
+const protectedMembers = Symbol("protected members");
+let _underConstruction = false;
 
 
-/*** helper functions ***/
-
-function defineProperty(object, propertyName, value, isWritable, isEnumerable, isConfigurable){
-	Object.defineProperty(object, propertyName, { value:value, writable:isWritable, enumerable:isEnumerable, configurable:isConfigurable });
+function Class(){
+	Object.defineProperty(this, protectedMembers, {
+		writable: false, enumerable: false, configurable: true,
+		value: {}
+	});
 }
-function defineNonEnumerableProperty(object, propertyName, value){
-	defineProperty(object, propertyName, value, true, false, true);
-}
 
-function isPrimitive(o){ return o === null || (typeof(o) !== "object" && typeof(o) !== "function"); }
+Class.prototype.constructor = Class;
 
+Object.defineProperty(Class.prototype, "toString", {
+	writable: true, enumerable: false, configurable: true,
+	value: function toString(){ return `[object ${this.constructor.name}]`; }
+});
 
-/*** base class ***/
-
-let _baseClass = function Class(){};
-
-defineNonEnumerableProperty(_baseClass.prototype, "toString", function toString(){ return "[object Class]"; });
+Object.defineProperty(Class, "extend", {
+	writable: true, enumerable: false, configurable: true,
+	value: extend
+});
 
 
 /**
  * Creates a new class that inherits from the super class.
  * 
- * @param {object} [options]
- * @param {string} [options.className] - Used as .name for the class function and in .toString() for instances of the class.
- * @param {function} [options.constructor] - Initializes a new instance of the class. Its first argument will be a function that can be used to call the constructor of the super class (like a `super` keyword). It must be called before using `this`, or to gain access to protected members of the super class. If not specified, the default constructor will call the constructor of the super class, passing any arguments.
- * @param {function} [options.function] - Returns a value when the class is called as a function instead of as a constructor (i.e., without using the 'new' keyword). If not specified, a TypeError will be thrown instead.
+ * @param {function} [constructor] - Initializes a new instance of the class. Its first argument will be a function that can be used to call the constructor of the super class (instead of the `super` keyword). It must be called in the constructor, it should be called before using `this`, and it must be called to gain access to protected members.
+ * @param {function} [applier] - Returns a value when the class is called without using the 'new' keyword. If not specified, a TypeError will be thrown instead of returning a value.
  * @return {function} - The new class.
  */
-let _extendFn = function extend(options){
+function extend(constructor, applier){
+	if(typeof this !== "function" || !(this === Class || this.prototype instanceof Class))
+		throw new TypeError("extend requires that 'this' be a Class constructor");
+	if(typeof constructor !== "function")
+		throw new TypeError("constructor is not a function");
+	if(!constructor.name)
+		throw new TypeError("constructor must be a named function");
+	if(arguments.length > 1 && typeof applier !== "function")
+		throw new TypeError("applier is not a function");
 	
-	if(options === void 0) options = {};
-	else if(isPrimitive(options)) throw new TypeError("options is not an object");
+	const superClass = this;
 	
-	let $className = options.className;
-	if($className === void 0 || String($className)===""){
-		if(options.constructor.name !== "constructor") $className = options.constructor.name;
-		$className = $className || this.name || "Class";
-	}
-	
-	let $constructor = options.constructor;
-	if($constructor === void 0){
-		//default contructor calls the constructor of the super class, passing any remaining arguments
-		$constructor = function ($super){ $super.apply(void 0, [].slice.call(arguments, 1)); };
-	}
-	else if(typeof($constructor) !== "function") throw new TypeError("options.constructor is not a function");
-	
-	let $function = options.function;
-	if($function === void 0){
-		$function = () => throw new TypeError(`${$className} class constructor cannot be invoked without 'new'`);
-	}
-	else if(typeof(options.function) !== "function") throw new TypeError("options.function is not a function");
-	
-	
-	/*** create the new constructor ***/
-	
-	let newClass = function Class(){
+	function constructorWrapper(...argumentsList){
+		const newInstance = this;
+		let _$superCalled = false;
 		
-		if(this && this instanceof newClass && (this.constructor === newClass.prototype.constructor || _initializing)){
-			//A new instance is being created; initialize it.
-			//This condition will be true in these cases:
-			//  1) The 'new' operator was used to instantiate this class
-			//  2) The 'new' operator was used to instantiate a subclass, and the subclass' constructor calls the constructor of its super class
-			//  3) The 'new' operator was used to instantiate a subclass, and the subclass' constructor includes something like `MySuperClass.call(this)`
-			//  4) Possibly if the prototype chain has been manipulated
-
-			let newInstance = this;
-
-			if(newInstance.constructor === newClass.prototype.constructor){
-				//this function is the constructor of the new instance (i.e., it's not a super class' constructor)
+		const $super = new Proxy(superClass, {
+			apply(target, thisArg, argumentsList){	//target===superClass
+				if(_$superCalled) throw new ReferenceError("Super constructor may only be called once");
+				_$superCalled = true;
 				
-				defineNonEnumerableProperty(newInstance, "constructor", newClass);
-			}
-			
-			let superFnCalled = false;
-			function $super(){
-				if(superFnCalled) throw new ReferenceError("Super constructor may only be called once");
-				superFnCalled = true;
-				
-				//initialize the instance using the super class
-				let protectedAccessors = newClass.prototype.constructor.apply(newInstance, arguments) || {};
-				
-				//add the protected value accessors to `$super`
-				defineProperty($super, "protected", {}, false, false, false);
-				for(const key in protectedAccessors){
-					$super.protected[key] = protectedAccessors[key];
+				target.apply(newInstance, argumentsList);
+			},
+			get(target, property, receiver){	//target===superClass
+				if(property === "_protected"){
+					if(!_$superCalled) throw new ReferenceError("Must call super constructor from derived constructor before accessing protected members");
+					return target[protectedMembers];
 				}
+				return Reflect.get(...arguments);
+			},
+			set(target, property, value, receiver){	//target===superClass
+				if(property === "_protected") return false;
+				return Reflect.set(...arguments);
+			},
+			deleteProperty(target, property){	//target===superClass
+				throw new ReferenceError("invalid delete involving super constructor");
 			}
-			
-			//construct the new instance
-			_initializing = true;
-			$constructor.apply(newInstance, [$super].concat([].slice.call(arguments)));
-			
-			if(!superFnCalled) throw new ReferenceError("Must call super constructor in derived class before accessing 'this' or returning from derived constructor");
-			
-			if(newInstance.constructor === newClass){
-				//this function is the constructor of the new instance
-				
-				_initializing = false;
-			}
-			else{
-				//this function is the constructor of a super class
-				
-				//generate protected value accessors for the subclass
-				let protectedAccessors = {};
-				for(const key in $super.protected){
-					Object.defineProperty(protectedAccessors, key, {
-						get: ()=>$super[key],
-						set: value=>($super[key] = value),
-						enumerable:true, configurable:false
-					});
-				}
-				return protectedAccessors;
-			}
-			
+		});
+		
+		_underConstruction = true;
+		try{
+			constructor.apply(newInstance, [$super, ...argumentsList]);
 		}
-		else{
-			//the 'new' operator was not used; it was just called as a function
-
-			return $function.apply(void 0, arguments);
-
+		finally{
+			_underConstruction = false;
 		}
 		
-	}
-	
-	//override .name
-	defineProperty(newClass, "name", $className, false, false, true);
-	
-	if(this.extend === _extendFn){
-		//the 'extend' method of the super class was not modified
+		if(!_$superCalled) throw new ReferenceError("Must call super constructor before returning from derived constructor");
 		
-		//make extend() a static method of the new class
-		defineNonEnumerableProperty(newClass, "extend", _extendFn);
+		return newInstance;
 	}
-
 	
-	/*** create the new prototype ***/
-
-	//An uninitialized instance of the super class will be the prototype of the new class.
-	//To create an instance without initializing it, we'll temporarily use an empty function as the constructor.
-	let emptyFn = function (){};
-	emptyFn.prototype = this.prototype;
-	let newPrototype = new emptyFn();
-	defineNonEnumerableProperty(newPrototype, "constructor", this);
+	const newClass = new Proxy(constructorWrapper, {
+		construct(target, argumentsList, newTarget){	//target===constructorWrapper, newTarget===the proxy itself (newClass)
+			const newInstance = Reflect.construct(...arguments);
+			newInstance.constructor = newTarget;
+			return newInstance;
+		},
+		apply(target, thisArg, argumentsList){	//target===constructorWrapper
+			if(_underConstruction){
+				return target.apply(thisArg, argumentsList);	//thisArg===the new instance
+			}
+			else if(applier){
+				return applier.apply(thisArg, argumentsList);
+			}
+			throw new TypeError(`Class constructor ${constructor.name} cannot be invoked without 'new'`);
+		}
+	});
 	
-	//override .toString()
-	defineNonEnumerableProperty(newPrototype, "toString", function toString(){ return `[object ${$className}]`; });
-
-	defineProperty(newClass, "prototype", newPrototype, false, false, false);
+	console.log("newClass:", newClass);
+	newClass.prototype = Object.create(superClass.prototype);
+	newClass.prototype.constructor = superClass;
+	console.log(newClass.prototype instanceof superClass);
 	
+	Object.defineProperty(newClass, "name", {
+		writable: false, enumerable: false, configurable: true,
+		value: constructor.name
+	});
+	
+	Object.defineProperty(newClass, "toString", {
+		writable: true, enumerable: false, configurable: true,
+		value: function toString(){ return constructor.toString(); }
+	});
+	
+	//make extend() a static method of the new class
+	Object.defineProperty(newClass, "extend", {
+		writable: true, enumerable: false, configurable: true,
+		value: extend
+	});
 	
 	return newClass;
-	
 }
 
-//make extend() a static method of Class
-defineNonEnumerableProperty(_baseClass, "extend", _extendFn);
 
 
-
-export { _baseClass as default };
+export { Class as default };
